69291e43ed31eabbb0f01f8a4f0fa7c7b3183fe2
abalux <baluxagustina@gmail.com>
2023-10-20 21:02:33 -0300

    agregue lo del file system y metodos delete y update

---
+++ productManager.js
@@ -0,0 +1,114 @@
+import { promises as fs } from 'fs';
+
+class Product {
+    constructor (title, description, price, thumbnail, code, stock) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
+    }
+}
+
+class ProductManager {
+    constructor() {
+        this.route = "products.json"
+        this.products = [];
+    }
+
+    async readProducts(){
+        try {
+            const content = await fs.readFile(this.route, "utf-8");
+            const productsJson = JSON.parse(content); 
+            this.products = productsJson
+        } catch (error) {
+            console.error("Error al leer el archivo JSON:", error);
+        }
+    };
+
+    async writeProducts(){
+        const productsJson = JSON.stringify(this.products, null, 2);
+        await fs.writeFile(this.route, productsJson);
+    };
+
+    async addProduct(title, description, price, thumbnail, code, stock){
+        await this.readProducts()
+        const codeExists = this.products.some(product => product.code === code)
+        if (codeExists){
+            console.error("A product with this code already exists")
+        }else if(!title || !description || !price || !thumbnail || !code || !stock){
+            console.error("All fields are required")
+        }else{
+            const product = new Product(title, description, price, thumbnail, code, stock);
+            product.id = this.products.length + 1
+            this.products.push(product);
+            await this.writeProducts()
+        }
+    }
+
+    async getProducts(){
+        await this.readProducts();
+        console.log(this.products);
+        return this.products
+    }
+
+    async getProductById(id){
+        await this.readProducts();
+        const productFound = this.products.find(product => product.id === Number(id))
+        if(productFound){
+            console.log(productFound)
+            await this.writeProducts();
+            return productFound;
+        }else{
+            throw new Error("There isn't a product with that id")
+        }
+    }
+    
+    async deleteProduct(id){
+        await this.readProducts();
+        const index = this.products.findIndex(product => product.id === Number(id))
+        if(index !== -1){
+            const deletedProduct = this.products.splice(index, 1);
+            await this.writeProducts();
+            return deletedProduct;
+        }else{
+            throw new Error("Delete error: Product not found");
+        }
+        
+    }
+
+    async updateProduct(id, paramName, newValue){
+        await this.readProducts();
+        const index = this.products.findIndex(product => product.id === Number(id));
+        if(index !== -1){
+            this.products[index][paramName] = newValue;
+            await this.writeProducts();
+            return this.products[index];
+        }else{
+            throw new Error("Update error: Product not found");
+        }
+        
+    }
+}
+
+    const manager = new ProductManager();
+    //testeo
+    (async () => {
+        await manager.getProducts();
+        manager.addProduct("Producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+        await manager.getProducts();
+        manager.addProduct("producto prueba", "este es un producto prueba", 200, "sin imagen", "abc123", 25);
+        manager.addProduct("producto prueba", "este es un producto prueba", 200, "sin imagen", "abc124");
+        await manager.getProductById(1);
+        manager.addProduct("producto prueba b", "este es un producto prueba b", 300, "sin imagen", "abc125", 25);
+        await manager.getProducts();
+        await manager.getProductById(2);
+        await manager.deleteProduct(1);
+        await manager.getProducts();
+        await manager.updateProduct(2, 'price', 30);
+        await manager.getProducts();
+    })();
+
+    
+
